02dd327a6816c9c76fe929f9d8445a5492339af2
Srivatsa Viswanathan <sri.umd@gmail.com>
2013-12-28 17:44:26 -0500

    blog

---
+++ blog_source/pelicanconf.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Srivatsa Ray'
+SITENAME = u'srivatsa'
+SITEURL = 'http://sri.so/blog'
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+# Blogroll
+LINKS =  (('Pelican', 'http://getpelican.com/'),
+          ('Python.org', 'http://python.org/'),
+          ('Jinja2', 'http://jinja.pocoo.org/'),
+          ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('facebook', 'https://www.facebook.com/srivatsaray'),
+          ('twitter', 'http://twitter.com/sricola'),
+          ('instagram', 'http://instagram.com/nofiltersri'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
